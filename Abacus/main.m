3ed734e90606e9f3eda500c3a4cee2f4289c0173
Sidhant Gandhi <sidhant.gandhi@gmail.com>
2014-01-24 20:49:40 -0800

    Initial Commit

---
+++ Abacus/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Abacus
+//
+//  Created by Sidhant Gandhi on 1/24/14.
+//  Copyright (c) 2014 Sidhant Gandhi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
